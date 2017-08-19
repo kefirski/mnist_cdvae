b34e12ef1154197f2a8eca0ab0b8f476933bcb61
kefirski <olavursky@gmail.com>
2017-08-19 15:48:43 +0300

    fixes

--- model/encoder.py
+++ model/encoder.py
@@ -7,33 +7,24 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
 
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 50, kernel_size=5, stride=2, padding=2),
+            nn.Conv2d(1, 20, kernel_size=4, stride=2, padding=1),
             nn.ELU(inplace=True),
-            nn.AvgPool2d(2),
 
-            nn.Conv2d(50, 80, kernel_size=4, stride=2, padding=2),
+            nn.Conv2d(20, 50, kernel_size=4, stride=2, padding=1),
             nn.ELU(inplace=True),
-            nn.AvgPool2d(2),
 
-            nn.Conv2d(80, 100, kernel_size=2, stride=1, padding=0),
+            nn.Conv2d(50, 80, kernel_size=4, stride=2, padding=1),
+            nn.ELU(inplace=True),
+
+            nn.Conv2d(80, 100, kernel_size=3, stride=1, padding=0),
             nn.ELU(),
 
-            # nn.Linear(784, 400),
-            # nn.ELU(inplace=True),
-            #
-            # nn.Linear(400, 300),
-            # nn.ELU(inplace=True),
-            #
-            # nn.Linear(300, 100),
-            # nn.ELU(inplace=True),
         )
 
         self.hidden_to_mu = nn.Linear(100, 80)
         self.hidden_to_logvar = nn.Linear(100, 80)
 
     def forward(self, input):
-        # input = input.view(-1, 28 * 28)
 
         hidden = self.conv(input).view(-1, 100)
-
         return self.hidden_to_mu(hidden), self.hidden_to_logvar(hidden)
